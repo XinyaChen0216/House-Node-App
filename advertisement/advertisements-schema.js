eb533635048283e7f7974f0a1d891cbb72943eb8
xinyachen0216 <chen.xinya1@northeastern.edu>
2023-06-25 18:43:43 -0700

    wrap ads page

--- advertisement/advertisements-schema.js
+++ advertisement/advertisements-schema.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 const advertisementsSchema = new mongoose.Schema({
     agent: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
-    image: String,
+    images: String,
+    username: String,
     overview: String,
 }, { collection: "advertisement", strict: false });
 export default advertisementsSchema;
--- advertisement/advertisements.controller.js
+++ advertisement/advertisements.controller.js
@@ -1,38 +1,60 @@
 import * as advertisementsDao from "./advertisements-dao.js";
+import * as fs from "fs";
 
 const findAdvertisements = async (req, res) => {
-    const advertisements = await advertisementsDao.findAdvertisements();
-    res.json(advertisements);
-}
+  const advertisements = await advertisementsDao.findAdvertisements();
+  let finalRes = [];
+  advertisements.forEach((ad) => {
+    const curr = fs.readFileSync(`./public/images/${ad.images}`);
+    let currImageString = new Buffer.from(curr).toString("base64");
+    let currAds = {
+      ...ad._doc,
+      imageString: currImageString,
+    };
+    finalRes.push(currAds);
+  });
+  res.json(finalRes);
+};
 
 const findAdvertisementById = async (req, res) => {
-    const id = req.params.aid;
-    const advertisement = await advertisementsDao.findAdvertisementById(id);
-    res.json(advertisement);
-  };
+  const id = req.params.aid;
+  const advertisement = await advertisementsDao.findAdvertisementById(id);
+  res.json(advertisement);
+};
 
 const createAdvertisement = async (req, res) => {
-    const newAdvertisement = await advertisementsDao.createAdvertisement(req.body);
-    res.json(newAdvertisement);
+  const newAdvertisement = await advertisementsDao.createAdvertisement(
+    req.body
+  );
+  const curr = fs.readFileSync(`./public/images/${newAdvertisement.images}`);
+  let currImageString = new Buffer.from(curr).toString("base64");
+  let currAds = {
+    ...newAdvertisement._doc,
+    imageString: currImageString,
+  };
+  res.json(currAds);
 };
 
 const deleteAdvertisement = async (req, res) => {
-    const id = req.params.aid;
-    const status = await advertisementsDao.deleteAdvertisement(id);
-    res.json(status);
-}
+  const id = req.params.aid;
+  const status = await advertisementsDao.deleteAdvertisement(id);
+  res.json(status);
+};
 
 const updateAdvertisement = async (req, res) => {
-    const advertisementIdToUpdate = req.params.aid;
-    const updates = req.body;
-    const status = await advertisementsDao.updateAdvertisement(advertisementIdToUpdate, updates);
-    res.json(status);
-}
+  const advertisementIdToUpdate = req.params.aid;
+  const updates = req.body;
+  const status = await advertisementsDao.updateAdvertisement(
+    advertisementIdToUpdate,
+    updates
+  );
+  res.json(status);
+};
 
 export default (app) => {
-    app.get('/api/advertisements', findAdvertisements)
-    app.get('/api/advertisements/:aid', findAdvertisementById)
-    app.post('/api/advertisements', createAdvertisement)
-    app.put('/api/advertisements/:aid', updateAdvertisement)
-    app.delete('/api/advertisements/:aid', deleteAdvertisement)
-}
+  app.get("/api/advertisements", findAdvertisements);
+  app.get("/api/advertisements/:aid", findAdvertisementById);
+  app.post("/api/advertisements", createAdvertisement);
+  app.put("/api/advertisements/:aid", updateAdvertisement);
+  app.delete("/api/advertisements/:aid", deleteAdvertisement);
+};
