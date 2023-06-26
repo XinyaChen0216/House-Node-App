6b98626815bac1601188cbc6d3984c9fbb46b652
xinyachen0216 <chen.xinya1@northeastern.edu>
2023-06-25 17:48:12 -0700

    update ads schema

--- advertisement/advertisements-schema.js
+++ advertisement/advertisements-schema.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 const advertisementsSchema = new mongoose.Schema({
     agent: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
-    first_name: String,
-    last_name:String,
     image: String,
-    phone: String,
-    email: String,
+    overview: String,
 }, { collection: "advertisement", strict: false });
 export default advertisementsSchema;
